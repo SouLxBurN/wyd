c5c279c94a7edf119e323e863fac09229b594053
Tristan Farmer <tristanfarmer@gmail.com>
2022-11-10 21:30:11 -0800

    fix: Actually clean up listeners

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -22,34 +22,60 @@ pub struct Client {
     id: String,
     addr: SocketAddr,
     connected: bool,
+    ws_write: Arc<Mutex<SplitSink<WebSocketStream<TcpStream>, Message>>>,
+    bs_listener: Option<JoinHandle<()>>,
 }
 
 impl Client {
-    pub fn new_client(id: String, addr: SocketAddr) -> Arc<RwLock<Client>> {
-        Arc::new(RwLock::new(Client{id, addr, connected: true}))
+    async fn new_client(
+        id: String,
+        addr: SocketAddr,
+        ws_write: SplitSink<WebSocketStream<TcpStream>, Message>,
+        brx: Receiver<ChatMessage>) -> Arc<RwLock<Client>> {
+
+        let client = Arc::new(RwLock::new(
+            Client{
+                id,
+                addr,
+                connected: true,
+                ws_write: Arc::new(Mutex::new(ws_write)),
+                bs_listener: None
+            }));
+
+        Self::broadcast_listener(client, brx).await
     }
 
-    pub fn broadcast_listener(client: Arc<RwLock<Client>>, mut ws_write: SplitSink<WebSocketStream<TcpStream>, Message>, mut brx: Receiver<ChatMessage>) {
-        tokio::spawn(async move {
-            while let Ok(msg) = brx.recv().await {
-                let cl = client.read().await;
-                if !cl.connected {
-                    break;
+    async fn broadcast_listener(client: Arc<RwLock<Client>>, mut brx: Receiver<ChatMessage>) -> Arc<RwLock<Client>> {
+        let local_client = client.clone();
+
+        client.write().await.register_bs_listener(
+            tokio::spawn(async move {
+                while let Ok(msg) = brx.recv().await {
+                    let cl = local_client.read().await;
+                    if cl.id != msg.client_id {
+                        let _write = cl.ws_write.lock().await.send(
+                            Message::text(
+                                format!("{} | {}",
+                                    msg.client_id, msg.message))
+                        ).await;
+                        // TODO Handle Errors: Backoff retries, before disconnecting.
+                    }
                 }
-                if cl.id != msg.client_id {
-                    ws_write.send(
-                        Message::text(
-                            format!("{}|{}",
-                                msg.client_id, msg.message))
-                    ).await;
-                }
-            }
-            eprintln!("broadcast_listener disconnected");
-        });
+                eprintln!("broadcast_listener disconnected");
+            })
+        );
+        client
     }
 
-    pub fn clean(&mut self) {
+    fn register_bs_listener(&mut self, bs_listener: JoinHandle<()>) {
+        self.bs_listener = Some(bs_listener);
+    }
+
+    fn clean(&mut self) {
         self.connected = false;
+        if let Some(listener) = self.bs_listener.take() {
+            listener.abort();
+        }
     }
 }
 
@@ -61,6 +87,7 @@ pub struct BroadcastServer {
 impl BroadcastServer {
     pub fn start() -> Self {
         let (btx, mut rtx) = broadcast::channel::<ChatMessage>(16);
+        // Register a listener for the server to log messages
         tokio::spawn(async move {
             while let Ok(msg) = rtx.recv().await {
                 tokio::io::stdout().write_all(
@@ -99,21 +126,21 @@ impl BroadcastServer {
 
         println!("Web Socket connection established {}", addr);
 
-        let (mut write, read) = ws_stream.split();
+        let (write, read) = ws_stream.split();
 
         let now = SystemTime::now();
         let now = now.duration_since(UNIX_EPOCH).expect("This is bad").as_millis();
         let client_id = now.to_string();
-        let client = Client::new_client(client_id, addr);
+        let client = Client::new_client(client_id, addr, write, btx.subscribe()).await;
 
         {
             let rwcl = client.read().await;
-            write.send(Message::Text(rwcl.id.clone())).await;
             let mut l = clients.lock().await;
             l.insert(rwcl.id.clone(), client.clone());
+            let client_id_msg = Message::Text(rwcl.id.clone());
+            rwcl.ws_write.lock().await.send(client_id_msg).await.unwrap();
         }
 
-        Client::broadcast_listener(client.clone(), write, btx.subscribe());
         read.try_filter(|msg| future::ready(msg.is_text() || msg.is_binary()))
             .for_each(|msg| async {
                 // "12345|This is my message"
